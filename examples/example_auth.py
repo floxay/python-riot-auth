e70f76002728b99005baccd6fe3157c03d4d9f02
Huba T <57007485+floxay@users.noreply.github.com>
2022-12-23 02:00:04 +0100

    Add project name

--- examples/example_auth.py
+++ examples/example_auth.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2022 Huba Tuba (floxay)
+# riot-auth Copyright (c) 2022 Huba Tuba (floxay)
 # Licensed under the MIT license. Refer to the LICENSE file in the project root for more information.
 
 import asyncio
