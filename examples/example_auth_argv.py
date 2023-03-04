e37f4a07eb06c68973d6161e44c96d470d2357c8
ultronxr <Ultronxr@users.noreply.github.com>
2023-03-04 12:05:24 +0800

    Add 'example_auth_argv.py' which uses system command line argument values to get username and password.

---
+++ examples/example_auth_argv.py
@@ -0,0 +1,27 @@
+# riot-auth Copyright (c) 2022 Huba Tuba (floxay)
+# Licensed under the MIT license. Refer to the LICENSE file in the project root for more information.
+
+import asyncio
+import sys
+
+import riot_auth
+
+# region asyncio.run() bug workaround for Windows, remove below 3.8 and above 3.10.6
+if sys.platform == "win32":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+# endregion
+
+username = sys.argv[1]
+password = sys.argv[2]
+CREDS = username, password
+
+auth = riot_auth.RiotAuth()
+asyncio.run(auth.authorize(*CREDS))
+
+print(f"Access Token Type: {auth.token_type}\n")
+print(f"Access Token: {auth.access_token}\n")
+print(f"Entitlements Token: {auth.entitlements_token}\n")
+print(f"User ID: {auth.user_id}")
+
+# Reauth using cookies. Returns a bool indicating whether the reauth attempt was successful.
+asyncio.run(auth.reauthorize())
