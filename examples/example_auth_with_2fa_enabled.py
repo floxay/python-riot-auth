8c34c515efc4ed270047ca15a91d85be4b746dd8
ev3nvy <12035264+ev3nvy@users.noreply.github.com>
2023-07-22 19:26:04 +0200

    chore: add `example_auth_with_2fa_enabled`

---
+++ examples/example_auth_with_2fa_enabled.py
@@ -0,0 +1,32 @@
+import asyncio
+import sys
+
+import aioconsole
+import riot_auth
+
+
+async def main() -> None:
+    CREDS = "USERNAME", "PASSWORD"
+
+    auth = riot_auth.RiotAuth()
+    await auth.authorize(*CREDS)
+
+    while auth.multifactor_status == True:
+        # fetching the code must be asynchronous or blocking
+        code = await aioconsole.ainput("Input 2fa code: ")
+        try:
+            await auth.authorize_mfa(code)
+        except riot_auth.RiotMultifactorAttemptError:
+            print("Invalid 2fa code, please try again")
+
+    print(f"Access Token Type: {auth.token_type}\n")
+    print(f"Access Token: {auth.access_token}\n")
+    print(f"Entitlements Token: {auth.entitlements_token}\n")
+    print(f"User ID: {auth.user_id}")
+
+# region asyncio.run() bug workaround for Windows, remove below 3.8 and above 3.10.6
+if sys.platform == "win32":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+# endregion
+
+asyncio.run(main())
