d64e366806516e56b3cfc944237a56269fe660b2
Huba Tuba <57007485+floxay@users.noreply.github.com>
2023-07-24 03:49:10 +0200

    remove license notice from files

--- riot_auth/auth_exceptions.py
+++ riot_auth/auth_exceptions.py
@@ -1,6 +1,3 @@
-# riot-auth Copyright (c) 2022 Huba Tuba (floxay)
-# Licensed under the MIT license. Refer to the LICENSE file in the project root for more information.
-
 __all__ = (
     "RiotAuthenticationError",
     "RiotAuthError",
